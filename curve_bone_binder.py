120a95cd1195b29224531a007adc1038c63b057c
MURATAGAWA Kei <muratagawa@gmail.com>
2022-07-21 07:44:22 +0900

    Fix: results change each time it was run

--- curve_bone_binder.py
+++ curve_bone_binder.py
@@ -6,7 +6,7 @@ import dataclasses
 class CBB_OT_bind(bpy.types.Operator):
     bl_idname = "object.cbb_bind"
     bl_label = "Bind"
-    bl_description = "Bind selected curve points to bones."
+    bl_description = "Bind selected curve points to new bones."
     bl_options = {'REGISTER', 'UNDO'}
 
     def execute(self, context):
@@ -24,8 +24,8 @@ class CurveBoneTable:
     coordinate: Vector
 
 
-# Show in Hook (Ctrl+H) context menu
-def show_in_hook(self, context):
+# Show in Hooks menu
+def show_in_menu(self, context):
     self.layout.operator(CBB_OT_bind.bl_idname, text="Hook to new bones")
 
 
@@ -73,8 +73,17 @@ def deselect_all_bones(armature):
         bone.select = False
 
 
+def switch_mode(mode):
+    bpy.ops.object.mode_set(mode=mode)
+
+
 # Bind the selected control points to the new bones.
 def bind_bones(self, context) -> bool:
+    # !!IMPORTANT!! Mesh/curve data is not synchronized in Edit mode.
+    # So you must switch to Object mode to access the latest data before operation.
+    # ref: https://docs.blender.org/api/current/info_gotcha.html#modes-and-mesh-access
+    switch_mode('OBJECT')
+
     if (not context.active_object
             or len(context.selected_objects) != 2):
         popup_message_box(
@@ -107,11 +116,12 @@ def bind_bones(self, context) -> bool:
 
         # Translate world coordinate to Armature space local coordinate
         target_armature_local_co = armature.matrix_world.inverted() @ target_world_co
+        bpy.context.scene.cursor.location = target_world_co
 
         # Add bone
-        bpy.ops.object.mode_set(mode='OBJECT')
+        switch_mode('OBJECT')
         bpy.context.view_layer.objects.active = armature
-        bpy.ops.object.mode_set(mode='EDIT')
+        switch_mode('EDIT')
 
         bone = armature.data.edit_bones.new(name="HookBone")
         bone.head = target_armature_local_co
@@ -119,15 +129,15 @@ def bind_bones(self, context) -> bool:
         bone_name = bone.name
 
         # Select the added bone in pose mode
-        bpy.ops.object.mode_set(mode='POSE')
+        switch_mode('POSE')
         deselect_all_bones(armature)
         armature.data.bones[bone_name].select = True
 
         # Add Hook modifier and assign the curve points to the bones
-        bpy.ops.object.mode_set(mode='OBJECT')
+        switch_mode('OBJECT')
         curve.select_set(True)
         bpy.context.view_layer.objects.active = curve
-        bpy.ops.object.mode_set(mode='EDIT')
+        switch_mode('EDIT')
         deselect_all_curve_points(curve)
 
         mod = curve.modifiers.new(name="Hook", type='HOOK')
@@ -137,20 +147,20 @@ def bind_bones(self, context) -> bool:
         curve.data.splines[item.spline_index].bezier_points[item.point_index].select_right_handle = True
         curve.data.splines[item.spline_index].bezier_points[item.point_index].select_left_handle = True
         bpy.ops.object.hook_assign(modifier=mod.name)
-        self.report({'INFO'}, "Bone=" + bone_name + ", Modifier=" + mod.name)
+        self.report({'INFO'}, "Hook created. (Bone=" + bone_name + ", Modifier=" + mod.name + ")")
 
-    bpy.ops.object.mode_set(mode='OBJECT')
+    switch_mode('OBJECT')
     return True
 
 
 def register_curve_bone_binder():
     bpy.utils.register_class(CBB_OT_bind)
-    bpy.types.VIEW3D_MT_hook.append(show_in_hook)
+    bpy.types.VIEW3D_MT_hook.append(show_in_menu)
 
 
 def unregister_curve_bone_binder():
     bpy.utils.unregister_class(CBB_OT_bind)
-    bpy.types.VIEW3D_MT_hook.remove(show_in_hook)
+    bpy.types.VIEW3D_MT_hook.remove(show_in_menu)
 
 
 if __name__ == "__main__":
